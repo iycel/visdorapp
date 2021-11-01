4a615b8852bf271aa1131f20b13658f1c31d68b9
iycel <ismailycel@gmail.com>
2021-11-01 21:39:12 +0300

    feat : admin dashboard

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=False)
+
+    def __str__(self):
+        return self.name
