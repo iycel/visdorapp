f648f37f5394758552df3188e9739817d86acb91
iycel <ismailycel@gmail.com>
2021-11-04 18:25:50 +0300

    feat: login/logout

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('',home, name='home'),
     path('about/',about, name='about'),
     path('teacher/',teacher, name='teacher'),
-    path('login/',auth_views.LoginView.as_view(template_name = "login.html"), name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name = "logout.html"), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name = "login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = "logout.html"), name='logout'),
 ]
